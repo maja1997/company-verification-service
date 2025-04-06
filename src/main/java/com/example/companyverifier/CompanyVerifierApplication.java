983a4fb8e8c7f5ee825e5bf729a521f1181dbf21
maja1997 <machy997@gmail.com>
2025-04-06 16:53:49 +0200

    chore: Set up Spring Boot project

---
+++ src/main/java/com/example/companyverifier/CompanyVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.example.companyverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyVerifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/companyverifier/CompanyVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.companyverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
